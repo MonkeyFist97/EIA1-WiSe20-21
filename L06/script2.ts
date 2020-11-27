f6f01ceed19ae2d171d9e3460da8057b5bf9f0c0
MonkeyFist97 <Camolukoey97@gmail.com>
2020-11-27 18:12:04 +0100

    L06 Base

---
+++ L06/script2.ts
@@ -0,0 +1,33 @@
+//Africa
+var africa08:number = 1028;
+var africa18:number = 1235.5;
+
+//South America
+var samerica08:number = 1132.6;
+var samerica18:number = 1261.5;
+
+//Europe
+var europa08:number = 4965.7;
+var europa18:number = 4209.3;
+
+//North America
+var namerica08:number = 6600.4;
+var namerica18:number = 6035.6;
+
+//Asia
+var asia08:number = 12954.7;
+var asia18:number = 16274.1;
+
+//Australia
+var australia08:number = 1993;
+var australia18:number = 2100.5;
+
+var allEmissions18:number = (africa18 + samerica18 + europa18 + namerica18 + asia18 + australia18);
+var hundred:number = 100;
+
+//Funktion
+
+function Werte(){
+
+    
+}
