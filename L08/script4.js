7334d83036066b0416e520dfe8ec43d67fd81373
MonkeyFist97 <Camolukoey97@gmail.com>
2020-12-11 21:53:28 +0100

    L08 vollendet

--- L08/script4.js
+++ L08/script4.js
@@ -19,65 +19,107 @@ function playSample(Sound) {
 // tslint:disable-next-line: typedef
 document.querySelector(".padA").addEventListener("click", function () {
     playSample(0);
+    recordingBeat(0);
 });
 //Sound C
 document.querySelector(".padC").addEventListener("click", function () {
     playSample(1);
+    recordingBeat(1);
 });
 //Sound F
 document.querySelector(".padF").addEventListener("click", function () {
     playSample(2);
+    recordingBeat(2);
 });
 //Sound G
 document.querySelector(".padG").addEventListener("click", function () {
     playSample(3);
+    recordingBeat(3);
 });
 //Sound hihat
 document.querySelector(".hihat").addEventListener("click", function () {
     playSample(4);
+    recordingBeat(4);
 });
 //Sound kick
 document.querySelector(".kick").addEventListener("click", function () {
     playSample(5);
+    recordingBeat(5);
 });
 //Sound laugh-1
 document.querySelector(".laugh-1").addEventListener("click", function () {
     playSample(6);
+    recordingBeat(6);
 });
 //Sound laugh-2
 document.querySelector(".laugh-2").addEventListener("click", function () {
     playSample(7);
+    recordingBeat(7);
 });
 //Sound laugh-2
 document.querySelector(".snare").addEventListener("click", function () {
     playSample(8);
+    recordingBeat(8);
 });
-//Record-Button leuchtet rot auf wenn "Aktiv"
+//Variablen der Buttons
+var playButton = document.getElementById("play");
+var stopButton = document.getElementById("stop");
 var recordButton = document.getElementById("record");
+var trashButton = document.getElementById("trash");
+//Record-Button leuchtet rot auf wenn "Aktiv"; zusätzlich Sounds aufnehmen mit dem boolean
+var recording;
 window.addEventListener("load", function () {
     recordButton.addEventListener("click", function () {
         if (recordButton.classList.contains("inactive")) {
             recordButton.classList.remove("inactive");
             recordButton.classList.add("active");
+            recording = true;
         }
         else if (recordButton.classList.contains("active")) {
             recordButton.classList.remove("active");
             recordButton.classList.add("inactive");
+            recording = false;
         }
     });
 });
+//Die Sounds aufnehmen - Die Töne in den Beat Array pushen
+function recordingBeat(Sound) {
+    if (recording == true) {
+        beat.push(Sound);
+    }
+}
+;
+//Der aufgenommene Loop wiederholt sich hiermit 
+var myInterval;
+var beat = [];
+var index = 0;
+function playLoop() {
+    myInterval = setInterval(function () {
+        if (index < beat.length) {
+            playSample(beat[index]);
+            index++;
+        }
+        else {
+            index = 0;
+        }
+    }, 300);
+}
+;
 //Play-Button transformiert zu Stop-Button
-var playButton = document.getElementById("play");
-var stopButton = document.getElementById("stop");
-playButton.addEventListener("click", function () {
-    togglePlayStop(this, stopButton);
-});
-stopButton.addEventListener("click", function () {
-    togglePlayStop(this, playButton);
-});
 function togglePlayStop(firstHTML, secondHTML) {
     firstHTML.classList.add("is-hidden");
     secondHTML.classList.remove("is-hidden");
 }
-//Funktion Recording
+playButton.addEventListener("click", function () {
+    togglePlayStop(this, stopButton);
+    playLoop();
+});
+stopButton.addEventListener("click", function () {
+    togglePlayStop(this, playButton);
+    clearInterval(myInterval);
+});
+//Aufgenommenen "Loop" löschen
+trashButton.addEventListener("click", function () {
+    beat = [];
+});
 //# sourceMappingURL=script4.js.map
--- L08/script4.ts
+++ L08/script4.ts
@@ -13,96 +13,137 @@ sample[7] = new Audio("./assets/laugh-2.mp3");
 sample[8] = new Audio("./assets/snare.mp3");
 
 
-
 //Funktion
 // tslint:disable-next-line: variable-name
 function playSample (Sound: number): void {
     sample[Sound].play ();
 }
 
+
 //Sound A
 // tslint:disable-next-line: typedef
 document.querySelector (".padA").addEventListener ("click" , function (): void {
     playSample (0);
+    recordingBeat (0);
 });
 
 //Sound C
 document.querySelector (".padC").addEventListener ("click" , function (): void {
     playSample (1);
+    recordingBeat (1);
 });
 
 //Sound F
 document.querySelector (".padF").addEventListener ("click" , function (): void {
     playSample (2);
+    recordingBeat (2);
 });
 
 //Sound G
 document.querySelector (".padG").addEventListener ("click" , function (): void {
     playSample (3);
+    recordingBeat (3);
 });
 
 //Sound hihat
 document.querySelector (".hihat").addEventListener ("click" , function (): void {
     playSample (4);
+    recordingBeat (4);
 });
 
 //Sound kick
 document.querySelector (".kick").addEventListener ("click" , function (): void {
     playSample (5);
+    recordingBeat (5);
 });
 
 //Sound laugh-1
 document.querySelector (".laugh-1").addEventListener ("click" , function (): void {
     playSample (6);
+    recordingBeat (6);
 });
 
 //Sound laugh-2
 document.querySelector (".laugh-2").addEventListener ("click" , function (): void {
     playSample (7);
+    recordingBeat (7);
 });
 
 //Sound laugh-2
 document.querySelector (".snare").addEventListener ("click" , function (): void {
     playSample (8);
+    recordingBeat (8);
+    
 });
 
 
-//Record-Button leuchtet rot auf wenn "Aktiv"
+//Variablen der Buttons
+var playButton: HTMLElement = document.getElementById ("play");
+var stopButton: HTMLElement = document.getElementById ("stop");
 var recordButton: HTMLElement = document.getElementById ("record");
+var trashButton: HTMLElement = document.getElementById ("trash");
+
+
+//Record-Button leuchtet rot auf wenn "Aktiv"; zusätzlich Sounds aufnehmen mit dem boolean
+let recording: boolean;
 
 window.addEventListener ("load" , function (): void {
     recordButton.addEventListener ( "click" , function (): void { 
     if (recordButton.classList.contains ("inactive")) {
         recordButton.classList.remove ("inactive");
         recordButton.classList.add ("active");
+        recording = true ;
     } else if (recordButton.classList.contains ("active")) {
         recordButton.classList.remove ("active");
         recordButton.classList.add ("inactive");   
+        recording = false ;
     }
 });
 });
+//Die Sounds aufnehmen - Die Töne in den Beat Array pushen
+
+function recordingBeat (Sound: number): void {
+    if (recording == true) {
+    beat.push(Sound);
+}};
+
+
+//Der aufgenommene Loop wiederholt sich hiermit 
+let myInterval: number;
+let beat: number [] = [];
+let index: number = 0;
+ 
+function playLoop (): void {
+    myInterval = setInterval (function () {
+
+    if (index < beat.length){
+        playSample(beat [index]) index++;
+    } else {index = 0;}
+    },      300);
+};
 
 
 //Play-Button transformiert zu Stop-Button
-var playButton: HTMLElement = document.getElementById ("play");
-var stopButton: HTMLElement = document.getElementById ("stop");
-
-playButton.addEventListener ( "click" , function (): void {
-    togglePlayStop (this, stopButton);
-});
-
-stopButton.addEventListener ( "click" , function (): void {
-    togglePlayStop (this, playButton);
-});
-
 function togglePlayStop (firstHTML: HTMLElement , secondHTML: HTMLElement): void {
     firstHTML.classList.add ("is-hidden");
     secondHTML.classList.remove ("is-hidden");
 }
+playButton.addEventListener ( "click" , function (): void {
+    togglePlayStop (this, stopButton);
+    playLoop ();
+});
+
+stopButton.addEventListener ( "click" , function (): void {
+    togglePlayStop (this, playButton);
+    clearInterval (myInterval);
+});
 
 
-//Funktion Recording
+//Aufgenommenen "Loop" löschen
 
+trashButton.addEventListener ("click" , function(): void {
+    beat = [];
+});
 
 
 
